b49a0f8eabb04b165cbbfd19316bf21d55c02d4a
Victor Pontis <victor@pont.is>
2022-06-21 18:31:07 -0400

    Update to be only english

--- src/_wordlists.js
+++ src/_wordlists.js
@@ -7,52 +7,43 @@ const wordlists = {};
 exports.wordlists = wordlists;
 let _default;
 exports._default = _default;
-try {
-    exports._default = _default = require('./wordlists/czech.json');
-    wordlists.czech = _default;
-}
-catch (err) { }
-try {
-    exports._default = _default = require('./wordlists/chinese_simplified.json');
-    wordlists.chinese_simplified = _default;
-}
-catch (err) { }
-try {
-    exports._default = _default = require('./wordlists/chinese_traditional.json');
-    wordlists.chinese_traditional = _default;
-}
-catch (err) { }
-try {
-    exports._default = _default = require('./wordlists/korean.json');
-    wordlists.korean = _default;
-}
-catch (err) { }
-try {
-    exports._default = _default = require('./wordlists/french.json');
-    wordlists.french = _default;
-}
-catch (err) { }
-try {
-    exports._default = _default = require('./wordlists/italian.json');
-    wordlists.italian = _default;
-}
-catch (err) { }
-try {
-    exports._default = _default = require('./wordlists/spanish.json');
-    wordlists.spanish = _default;
-}
-catch (err) { }
-try {
-    exports._default = _default = require('./wordlists/japanese.json');
-    wordlists.japanese = _default;
-    wordlists.JA = _default;
-}
-catch (err) { }
-try {
-    exports._default = _default = require('./wordlists/portuguese.json');
-    wordlists.portuguese = _default;
-}
-catch (err) { }
+// try {
+//   _default = require('./wordlists/czech.json');
+//   wordlists.czech = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/chinese_simplified.json');
+//   wordlists.chinese_simplified = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/chinese_traditional.json');
+//   wordlists.chinese_traditional = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/korean.json');
+//   wordlists.korean = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/french.json');
+//   wordlists.french = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/italian.json');
+//   wordlists.italian = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/spanish.json');
+//   wordlists.spanish = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/japanese.json');
+//   wordlists.japanese = _default as string[];
+//   wordlists.JA = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/portuguese.json');
+//   wordlists.portuguese = _default as string[];
+// } catch (err) {}
 try {
     exports._default = _default = require('./wordlists/english.json');
     wordlists.english = _default;
--- ts_src/_wordlists.ts
+++ ts_src/_wordlists.ts
@@ -3,43 +3,43 @@
 // based on what is present. (Bundles may remove wordlists they don't need)
 const wordlists: { [index: string]: string[] } = {};
 let _default: string[] | undefined;
-try {
-  _default = require('./wordlists/czech.json');
-  wordlists.czech = _default as string[];
-} catch (err) {}
-try {
-  _default = require('./wordlists/chinese_simplified.json');
-  wordlists.chinese_simplified = _default as string[];
-} catch (err) {}
-try {
-  _default = require('./wordlists/chinese_traditional.json');
-  wordlists.chinese_traditional = _default as string[];
-} catch (err) {}
-try {
-  _default = require('./wordlists/korean.json');
-  wordlists.korean = _default as string[];
-} catch (err) {}
-try {
-  _default = require('./wordlists/french.json');
-  wordlists.french = _default as string[];
-} catch (err) {}
-try {
-  _default = require('./wordlists/italian.json');
-  wordlists.italian = _default as string[];
-} catch (err) {}
-try {
-  _default = require('./wordlists/spanish.json');
-  wordlists.spanish = _default as string[];
-} catch (err) {}
-try {
-  _default = require('./wordlists/japanese.json');
-  wordlists.japanese = _default as string[];
-  wordlists.JA = _default as string[];
-} catch (err) {}
-try {
-  _default = require('./wordlists/portuguese.json');
-  wordlists.portuguese = _default as string[];
-} catch (err) {}
+// try {
+//   _default = require('./wordlists/czech.json');
+//   wordlists.czech = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/chinese_simplified.json');
+//   wordlists.chinese_simplified = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/chinese_traditional.json');
+//   wordlists.chinese_traditional = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/korean.json');
+//   wordlists.korean = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/french.json');
+//   wordlists.french = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/italian.json');
+//   wordlists.italian = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/spanish.json');
+//   wordlists.spanish = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/japanese.json');
+//   wordlists.japanese = _default as string[];
+//   wordlists.JA = _default as string[];
+// } catch (err) {}
+// try {
+//   _default = require('./wordlists/portuguese.json');
+//   wordlists.portuguese = _default as string[];
+// } catch (err) {}
 try {
   _default = require('./wordlists/english.json');
   wordlists.english = _default as string[];
